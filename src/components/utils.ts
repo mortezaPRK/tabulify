a857a9ecaa8a8fb83ce6fa557a107d1e8d8a6905
Armin <ar.basirian@gmail.com>
2024-11-18 23:49:40 +0100

    Fix getUniqueId

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,1 +1,2 @@
-export const getUniqueId = (value: unknown) => [value, Math.random()].join('-');
+export const getUniqueId = (prefix: unknown): string =>
+  `${prefix ?? 'id'}-${Date.now()}-${Math.random().toString(36).substring(2, 9)}`;
